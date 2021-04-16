e12707439d8ceab5e2e70be03eae92de93069f25
manuelfraile <manu.fra.ro@gmail.com>
2021-04-16 23:23:32 +0200

    DONEEE

--- assignment2/Assignment2.py
+++ assignment2/Assignment2.py
@@ -1,6 +1,75 @@
+import numpy as np
+
 from functions import *
 import matplotlib.pyplot as plt
 import math
+
+
+def loadOneBatchForEach():
+
+    training = LoadBatch('cifar-10-batches-py/data_batch_1')
+    trainX = normalise(training[b'data'].T.astype(int))
+    trainy = [int(i) for i in training[b'labels']]
+    trainY = (np.eye(10)[trainy]).T
+
+    validation = LoadBatch('cifar-10-batches-py/data_batch_2')
+    validX = normalise(validation[b'data'].T.astype(int))
+    validy = [int(i) for i in validation[b'labels']]
+    validY = (np.eye(10)[validy]).T
+
+    testing = LoadBatch('cifar-10-batches-py/test_batch')
+    testX = normalise(testing[b'data'].T.astype(int))
+    testy = [int(i) for i in testing[b'labels']]
+    testY = (np.eye(10)[testy]).T
+
+    return trainX, trainy, trainY, validX, validy, validY, testX, testy, testY
+
+
+def loadAllBatches():
+
+    training = LoadBatch('cifar-10-batches-py/data_batch_1')
+    trainX_1 = normalise(training[b'data'].T.astype(int))
+    trainy_1 = [int(i) for i in training[b'labels']]
+    trainY_1 = (np.eye(10)[trainy_1]).T
+
+    training = LoadBatch('cifar-10-batches-py/data_batch_2')
+    trainX_2 = normalise(training[b'data'].T.astype(int))
+    trainy_2 = [int(i) for i in training[b'labels']]
+    trainY_2 = (np.eye(10)[trainy_2]).T
+
+    training = LoadBatch('cifar-10-batches-py/data_batch_3')
+    trainX_3 = normalise(training[b'data'].T.astype(int))
+    trainy_3 = [int(i) for i in training[b'labels']]
+    trainY_3 = (np.eye(10)[trainy_3]).T
+
+    training = LoadBatch('cifar-10-batches-py/data_batch_4')
+    trainX_4 = normalise(training[b'data'].T.astype(int))
+    trainy_4 = [int(i) for i in training[b'labels']]
+    trainY_4 = (np.eye(10)[trainy_4]).T
+
+    training = LoadBatch('cifar-10-batches-py/data_batch_5')
+    trainX_5 = normalise(training[b'data'].T.astype(int))
+    trainy_5 = [int(i) for i in training[b'labels']]
+    trainY_5 = (np.eye(10)[trainy_5]).T
+
+    trainX = np.concatenate((trainX_1, trainX_2, trainX_3, trainX_4, trainX_5), axis=1)
+    trainY = np.concatenate((trainY_1, trainY_2, trainY_3, trainY_4, trainY_5), axis=1)
+    trainy = np.concatenate((trainy_1, trainy_2, trainy_3, trainy_4, trainy_5))
+
+    validX = trainX[:, -1000:]
+    validY = trainY[:, -1000:]
+    validy = trainy[-1000:]
+
+    trainX = trainX[:, :-1000]
+    trainY = trainY[:, :-1000]
+    trainy = trainy[:-1000]
+
+    testing = LoadBatch('cifar-10-batches-py/test_batch')
+    testX = normalise(testing[b'data'].T.astype(int))
+    testy = [int(i) for i in testing[b'labels']]
+    testY = (np.eye(10)[testy]).T
+
+    return trainX, trainy, trainY, validX, validy, validY, testX, testy, testY
 
 
 def normalise(matrix):
@@ -146,7 +215,7 @@ def saveData(totalCost, totalLoss, totalAcc, t):
     plt.xlabel("update step")
     plt.ylabel("cost")
     plt.legend()
-    plt.savefig('Result_Pics/cost1.png')
+    plt.savefig('Result_Pics/BNcost.png')
 
     plt.figure(2)
     plt.plot(t, totalLoss[0], label='training loss')
@@ -155,7 +224,7 @@ def saveData(totalCost, totalLoss, totalAcc, t):
     plt.xlabel("update step")
     plt.ylabel("loss")
     plt.legend()
-    plt.savefig('Result_Pics/loss1.png')
+    plt.savefig('Result_Pics/BNloss.png')
 
     plt.figure(3)
     plt.plot(t, totalAcc[0], label='training accuracy')
@@ -164,25 +233,14 @@ def saveData(totalCost, totalLoss, totalAcc, t):
     plt.xlabel("update step")
     plt.ylabel("accuracy")
     plt.legend()
-    plt.savefig('Result_Pics/accuracy1.png')
+    plt.savefig('Result_Pics/BNaccuracy.png')
 
 
 if __name__ == "__main__":
 
-    training = LoadBatch('cifar-10-batches-py/data_batch_1')
-    trainX = normalise(training[b'data'].T.astype(int))
-    trainy = [int(i) for i in training[b'labels']]
-    trainY = (np.eye(10)[trainy]).T
+    #trainX, trainy, trainY, validX, validy, validY, testX, testy, testY = loadOneBatchForEach()
+    trainX, trainy, trainY, validX, validy, validY, testX, testy, testY = loadAllBatches()
 
-    validation = LoadBatch('cifar-10-batches-py/data_batch_2')
-    validX = normalise(validation[b'data'].T.astype(int))
-    validy = [int(i) for i in validation[b'labels']]
-    validY = (np.eye(10)[validy]).T
-
-    testing = LoadBatch('cifar-10-batches-py/test_batch')
-    testX = normalise(testing[b'data'].T.astype(int))
-    testy = [int(i) for i in testing[b'labels']]
-    testY = (np.eye(10)[testy]).T
 
     # Dimension parameters
     d = trainX.shape[0]
@@ -205,14 +263,20 @@ if __name__ == "__main__":
     n_epochs = 200
     GDparams = {'n_batch': n_batch, 'eta': eta, 'n_epochs': n_epochs}
 
-    lamda = 0.01
+    lamda = 0.0010283387653801572
     h_param = 0.000001
+
+    # lambda regularisation
+    l_min = -3
+    l_max = -1.5
+    best_accuracy = 0
+    best_lamda = 0
 
     # TRAIN THE MODEL
     t = -1
     total_t = 0
     accuracy = 0
-    cycles = 2
+    cycles = 3
 
     totalTrainCost = []
     totalValidateCost = []
@@ -227,6 +291,21 @@ if __name__ == "__main__":
     etaEv = []
 
     N = int(trainX.shape[1] / n_batch)
+    '''''
+    for i in range(20):
+
+        t = -1
+        total_t = 0
+
+        print('******************')
+        print('Network candidate' + str(i))
+        print('******************')
+
+        l_epoch = l_min + (l_max-l_min)*np.random.rand(1, 1)[0][0]
+        lamda = 10**l_epoch
+
+    '''''
+
     for n in range(n_epochs):
 
         trainCost = ComputeCost(trainX, trainY, W, b, lamda)
@@ -276,18 +355,38 @@ if __name__ == "__main__":
 
         if total_t >= cycles * 2*n_s:
             break
+        ''''
+        accuracy_epoch = ComputeAccuracy(validX, validy, W, b)
+
+        if accuracy_epoch > best_accuracy:
+            best_accuracy = accuracy_epoch
+            best_lamda = lamda
+        '''
+
+        #print('\n')
+        ''''
+        file = open("Result_Pics/hyper_parameters_fine.txt", "a")
+        file.write('\nCandidate number ' + str(i) + ' has an accuracy of ' + str(accuracy_epoch) + ' for a lambda of ' + str(lamda))
+        file.close()
+        '''''
+
+
+    #print('The best accuracy reached is ' + str(best_accuracy) + ' for a lambda of ' + str(best_lamda))
+    print('\n\nThe reached accuracy is: ')
+    print(ComputeAccuracy(testX, testy, W, b))
 
     totalCost = [totalTrainCost, totalValidateCost]
     totalLoss = [totalTrainLoss, totalValidateLoss]
     totalAcc = [totalTrainAcc, totalValidateAcc]
 
-    print(ComputeAccuracy(testX, testy, W, b))
+    # print(ComputeAccuracy(testX, testy, W, b))
 
     saveData(totalCost, totalLoss, totalAcc, totalT)
-
+    '''''
     plt.close("all")
     plt.figure(4)
     plt.plot(list(range(total_t)), etaEv)
     plt.xlabel("update step")
     plt.ylabel("eta")
     plt.show()
+    '''''
